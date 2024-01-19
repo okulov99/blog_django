b9f2aecfc917cb8ec31508651d1be6f78a7f12b8
okulov99 <okulov.e7@gmail.com>
2024-01-19 12:27:08 +0300

    Теперь страница с добавлением поста доступна только для авторизованных пользователей

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -97,3 +97,4 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- blog/main/views.py
+++ blog/main/views.py
@@ -1,4 +1,4 @@
-#from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import AddPostForm
@@ -6,19 +6,25 @@ from django.views.generic import ListView, DetailView, CreateView
 
 
 class Home(ListView):
+    """Класс отвечает за отображение главной страницы сайта"""
     model = Post
     template_name = 'main/main.html'
     context_object_name = 'post_list'
-    paginate_by = 1
+    paginate_by = 3
 
 
 class PostDetail(DetailView):
+    """Класс отвечает за отображение страниц с деталями поста"""
     model = Post
     template_name = 'main/post_detail.html'
     context_object_name = 'post'
 
 
-class AddPost(CreateView):
+class AddPost(LoginRequiredMixin, CreateView):
+    """
+    Класс отвечает за форму с добавлением постов.
+    Страница с формой доступна только для авторизованных пользователей.
+    """
     form_class = AddPostForm
     template_name = 'main/add_post.html'
     success_url = reverse_lazy('home')
