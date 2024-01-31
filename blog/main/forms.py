4cb1ea3f413ca88fa5ceba776daf60ad1c60fba9
okulov99 <okulov.e7@gmail.com>
2024-01-31 14:21:02 +0300

    Мелкие улучшения

--- blog/main/forms.py
+++ blog/main/forms.py
@@ -1,8 +1,12 @@
+import datetime
 from django import forms
 from .models import Post, Comment
 
 
 class AddPostForm(forms.ModelForm):
+    this_year = datetime.date.today().year
+    date = forms.DateField(initial=datetime.date.today,
+                                 label='Дата публикации', disabled=True)
     class Meta:
         model = Post
         fields = ['title', 'content', 'date', 'img']
